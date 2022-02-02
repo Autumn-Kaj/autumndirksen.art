87627368e38f99c6eeccf0f28be9afdc7b1ab11a
Autumn-Kaj <71853844+Autumn-Kaj@users.noreply.github.com>
2022-02-02 10:30:10 -0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('#backtop').click(function(){
+        window.scroll({top: 0, duration: 4000, behavior: 'smooth'});
+    });
+});
+    
+    ScrollReveal().reveal('#title-reveal', { distance: '150%',
+    origin: 'bottom', duration: 2000});
+    ScrollReveal().reveal('#sub-reveal', { distance: '120%',
+    origin: 'bottom', duration: 2000, delay: 500});
+    ScrollReveal().reveal('#arrow-reveal', { distance: '110%',
+    origin: 'bottom', duration: 2000, delay: 1000 });
+
+    ScrollReveal().reveal('#creatures', { distance: '30px',
+    origin: 'bottom', duration: 1000});
+    ScrollReveal().reveal('#characters', { distance: '30px',
+    origin: 'bottom', duration: 1000, delay: 400});
+    ScrollReveal().reveal('#illustrations', { distance: '30px',
+    origin: 'bottom', duration: 1000, delay: 800 });
+    
+    ScrollReveal().reveal('#title-slide', { distance: '30px',
+    origin: 'right', duration: 2000, });
